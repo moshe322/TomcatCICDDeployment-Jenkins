f00b75085a64d98b0ad5edd32fc467e696c97908
Abhishek@Newton <abhishek.padmanabhan@gmail.com>
2021-06-27 21:28:27 +0530

    Maven Web Application

---
+++ src/main/java/com/caltech/mavenwebapplication/MavenwebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.mavenwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenwebapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/mavenwebapplication/MavenwebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.mavenwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenwebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
